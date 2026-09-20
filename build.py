2994df90d407daf318bab87fa91daaf1b09e55d0
Jinseok Lee <jinsuk9095@gmail.com>
2026-09-20 16:30:48 -0400

    Update portrait image in build.py

--- build.py
+++ build.py
@@ -59,7 +59,7 @@ about=f'''
 
 <div class="about-grid">
   <aside>
-    <img src="assets/portrait.jpg" alt="Jinseok Lee" width="361" height="421">
+    <img src="assets/portrait_about.jpg" alt="Jinseok Lee" width="361" height="421">
 
     <div class="links" style="margin-top:22px;gap:12px 16px;">
       {link(D['scholar'],'Google Scholar')}
